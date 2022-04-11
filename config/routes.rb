5197c66ccc65b0a57d3ad3dfff3d63cdcf4a4945
伊東和成 <itokazunari5375@icloud.com>
2022-04-11 23:06:17 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-    registrations: 'auth/registrations'
-  }
+  namespace :api, format: "json" do
+    namespace :v1 do
+      mount_devise_token_auth_for "User", at: "auth", controllers: {
+        registrations: "api/v1/auth/registrations"
+      }
+    end
+  end
 end
